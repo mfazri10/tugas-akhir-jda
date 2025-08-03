91691cd5960a998b5b83fc87f9ba3f22b385efcf
Muhammad Fazri <muhammadfazri10@gmail.com>
2025-08-03 18:49:51 +0700

    feat : add project into github

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white fllex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        navbar
+      </div>
+    </div>
+  );
+}
