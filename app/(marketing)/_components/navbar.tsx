839f19db5cb98248b2d6fbfb4ff7f0786917b9ff
Muhammad Fazri <muhammadfazri10@gmail.com>
2025-08-06 09:00:29 +0700

    fix : update

--- app/(marketing)/_components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white fllex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-        navbar
-      </div>
-    </div>
-  );
-}
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,16 +1,14 @@
-import Navbar from "./_components/navbar";
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
 
-export default function MarketingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-slate-100">
+    <div className="bg-slate-100 h-full">
       <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
-      {/* Footer */}
+      <Footer />
     </div>
   );
 };
 
+export default MarketingLayout;
