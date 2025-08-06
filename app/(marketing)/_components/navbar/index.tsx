7327c9f31985ef666e0d2950a186f01d1387826e
Muhammad Fazri <muhammadfazri10@gmail.com>
2025-08-06 09:18:00 +0700

    fix : perbaikan Judul

--- app/(marketing)/_components/navbar/index.tsx
+++ app/(marketing)/_components/navbar/index.tsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
             <Link href="/sign-in">Login</Link>
           </Button>
           <Button size="sm" asChild>
-            <Link href="/sign-up">Get DadaRello for free</Link>
+            <Link href="/sign-up">Get FazTask for free</Link>
           </Button>
         </div>
       </div>
